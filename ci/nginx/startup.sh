9e4ed44b6dec273170474bb991d05de52f781c25
Jarek Gawor <jgawor@gmail.com>
2019-08-23 16:09:42 -0400

    duh

--- ci/nginx/startup.sh
+++ ci/nginx/startup.sh
@@ -3,7 +3,7 @@
 cp -R /opt/index-src/* /opt/www/public
 
 # Replace the resource paths in index yaml files to match the specified external URL
-find /opt/www/public -name '*.yaml' -exec -r "s|https://(.*)/(.*)$|$EXTERNAL_URL/\2|" {} \;
+find /opt/www/public -name '*.yaml' -exec sed -r "s|https://(.*)/(.*)$|$EXTERNAL_URL/\2|" {} \;
 
 if [ -z "${DRY_RUN}" ]
 then
