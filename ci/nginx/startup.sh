bb7a82931351c5245d2d51e47715d89259fa7952
Jarek Gawor <jgawor@gmail.com>
2019-08-23 21:17:12 -0400

    fix user

--- ci/nginx/startup.sh
+++ ci/nginx/startup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-cp -R /opt/index-src/* /opt/www/public
-
 # Replace the resource paths in index yaml files to match the specified external URL
 find /opt/www/public -name '*.yaml' -exec sed -i -r "s|https://(.*)/(.*)$|$EXTERNAL_URL/\2|" {} \;
 
